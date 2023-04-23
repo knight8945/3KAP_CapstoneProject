8235ef81154a4d76282bcdcfe7e757e9b4163485
son7877 <79892287+son7877@users.noreply.github.com>
2023-04-23 16:18:09 +0900

    Androidphone-Build

---
+++ AR Fairytale/introScene_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,121 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.String VuforiaLicense::GetLicenseKey()
+extern void VuforiaLicense_GetLicenseKey_m8E6E03709CDB6968425A93FB9D016DA6AFC6C91B (void);
+// 0x00000002 System.Void VuforiaLicense::.ctor()
+extern void VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B (void);
+// 0x00000003 System.Void DisplayFPS::Awake()
+extern void DisplayFPS_Awake_m0ADFAD9AE5EAE5B8DCA0124AEF0890F75670C5C4 (void);
+// 0x00000004 System.Void DisplayFPS::Start()
+extern void DisplayFPS_Start_m09337BCF14392F6CB85C2C9F8A15B50E684C086B (void);
+// 0x00000005 System.Void DisplayFPS::Update()
+extern void DisplayFPS_Update_m4F218976FF34C68C391F478434E43D8E2FCABE7D (void);
+// 0x00000006 System.Void DisplayFPS::CalculateCurrentFPS()
+extern void DisplayFPS_CalculateCurrentFPS_mB0DAE0A006C52E505E53AB6AD7B13FE711B421D4 (void);
+// 0x00000007 System.Collections.IEnumerator DisplayFPS::ShowFPS()
+extern void DisplayFPS_ShowFPS_mA02AC8E3F27617779F8DF673A9F477067BCBADD4 (void);
+// 0x00000008 System.Void DisplayFPS::.ctor()
+extern void DisplayFPS__ctor_m7F12611998E32AD2F57B678C9B7DC16C4FE72170 (void);
+// 0x00000009 System.Void DisplayFPS/<ShowFPS>d__11::.ctor(System.Int32)
+extern void U3CShowFPSU3Ed__11__ctor_mCB31E79FBF4BFFB4861F08E9FC144A9159C7829E (void);
+// 0x0000000A System.Void DisplayFPS/<ShowFPS>d__11::System.IDisposable.Dispose()
+extern void U3CShowFPSU3Ed__11_System_IDisposable_Dispose_m66286264D9881E60863F984A267EAA5DAD7C8D77 (void);
+// 0x0000000B System.Boolean DisplayFPS/<ShowFPS>d__11::MoveNext()
+extern void U3CShowFPSU3Ed__11_MoveNext_mF94473F492FFD9B46FBEF3B469901FECE2479AB0 (void);
+// 0x0000000C System.Object DisplayFPS/<ShowFPS>d__11::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CShowFPSU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m6EDAC4435815D1A9487015755D9B73BE8BDB26CB (void);
+// 0x0000000D System.Void DisplayFPS/<ShowFPS>d__11::System.Collections.IEnumerator.Reset()
+extern void U3CShowFPSU3Ed__11_System_Collections_IEnumerator_Reset_m1E65B77CB5AFAF445C8E92C943451924C551F97E (void);
+// 0x0000000E System.Object DisplayFPS/<ShowFPS>d__11::System.Collections.IEnumerator.get_Current()
+extern void U3CShowFPSU3Ed__11_System_Collections_IEnumerator_get_Current_m6F2CAD216B6B1E4C4884A09D2E33EBFCB747D58F (void);
+// 0x0000000F System.Void LightFlicker::Start()
+extern void LightFlicker_Start_mA3559B9981E0CFB07520107ABCD75EB33B8F11BA (void);
+// 0x00000010 System.Void LightFlicker::Update()
+extern void LightFlicker_Update_mF94A41EE2492BC65C7D93D30C908E023C3D53BD7 (void);
+// 0x00000011 System.Void LightFlicker::ChangeRandomFlickerLightIntensity()
+extern void LightFlicker_ChangeRandomFlickerLightIntensity_mC8440E90D912F7E3A6C62E1A0F2BFB2D03C8BCCB (void);
+// 0x00000012 System.Void LightFlicker::ChangeAnimatedFlickerLightIntensity()
+extern void LightFlicker_ChangeAnimatedFlickerLightIntensity_m7CE9D495CAC0B7AF445881FC5839DFCA1A446027 (void);
+// 0x00000013 System.Void LightFlicker::.ctor()
+extern void LightFlicker__ctor_m9225E8532FCC76F9693B20A38476D058137B5FAB (void);
+// 0x00000014 System.Void LightFlicker::.cctor()
+extern void LightFlicker__cctor_m1047F734C2944D29A3ECD32199768E642C41C0CD (void);
+static Il2CppMethodPointer s_methodPointers[20] = 
+{
+	VuforiaLicense_GetLicenseKey_m8E6E03709CDB6968425A93FB9D016DA6AFC6C91B,
+	VuforiaLicense__ctor_m094B92B3323DFFD79ADB696474D5D158C95E291B,
+	DisplayFPS_Awake_m0ADFAD9AE5EAE5B8DCA0124AEF0890F75670C5C4,
+	DisplayFPS_Start_m09337BCF14392F6CB85C2C9F8A15B50E684C086B,
+	DisplayFPS_Update_m4F218976FF34C68C391F478434E43D8E2FCABE7D,
+	DisplayFPS_CalculateCurrentFPS_mB0DAE0A006C52E505E53AB6AD7B13FE711B421D4,
+	DisplayFPS_ShowFPS_mA02AC8E3F27617779F8DF673A9F477067BCBADD4,
+	DisplayFPS__ctor_m7F12611998E32AD2F57B678C9B7DC16C4FE72170,
+	U3CShowFPSU3Ed__11__ctor_mCB31E79FBF4BFFB4861F08E9FC144A9159C7829E,
+	U3CShowFPSU3Ed__11_System_IDisposable_Dispose_m66286264D9881E60863F984A267EAA5DAD7C8D77,
+	U3CShowFPSU3Ed__11_MoveNext_mF94473F492FFD9B46FBEF3B469901FECE2479AB0,
+	U3CShowFPSU3Ed__11_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m6EDAC4435815D1A9487015755D9B73BE8BDB26CB,
+	U3CShowFPSU3Ed__11_System_Collections_IEnumerator_Reset_m1E65B77CB5AFAF445C8E92C943451924C551F97E,
+	U3CShowFPSU3Ed__11_System_Collections_IEnumerator_get_Current_m6F2CAD216B6B1E4C4884A09D2E33EBFCB747D58F,
+	LightFlicker_Start_mA3559B9981E0CFB07520107ABCD75EB33B8F11BA,
+	LightFlicker_Update_mF94A41EE2492BC65C7D93D30C908E023C3D53BD7,
+	LightFlicker_ChangeRandomFlickerLightIntensity_mC8440E90D912F7E3A6C62E1A0F2BFB2D03C8BCCB,
+	LightFlicker_ChangeAnimatedFlickerLightIntensity_m7CE9D495CAC0B7AF445881FC5839DFCA1A446027,
+	LightFlicker__ctor_m9225E8532FCC76F9693B20A38476D058137B5FAB,
+	LightFlicker__cctor_m1047F734C2944D29A3ECD32199768E642C41C0CD,
+};
+static const int32_t s_InvokerIndices[20] = 
+{
+	9741,
+	6388,
+	6388,
+	6388,
+	6388,
+	6388,
+	6272,
+	6388,
+	4977,
+	6388,
+	6176,
+	6272,
+	6388,
+	6272,
+	6388,
+	6388,
+	6388,
+	6388,
+	6388,
+	9780,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	20,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
