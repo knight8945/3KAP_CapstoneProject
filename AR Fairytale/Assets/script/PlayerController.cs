7f42335b1a2f168da2533db382f0b03dc2f1cdfc
201713799KimTaeYoung <126853336+201713799KimTaeYoung@users.noreply.github.com>
2023-04-30 19:37:08 +0900

    김태영
    카메라 범위 제한/ 캐릭터 이동 제한/
    맵 이동(아직 일방통행) 구현

--- AR Fairytale/Assets/script/PlayerController.cs
+++ AR Fairytale/Assets/script/PlayerController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-
     Animator animator;
     string animationState = "AnimationState";
+
+    [SerializeField]
+    Transform playerTransform;
+    [SerializeField]
+    Transform cameraTransform;
 
     enum States
     {
@@ -21,6 +25,8 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         animator = GetComponent<Animator>();
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+
     }
     void Update()
     {
@@ -48,7 +54,54 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetInteger(animationState, (int)States.stop);
         }
+        // main -> 빨간모자 집 이동
+        if (playerTransform.position.y < 2.8 && playerTransform.position.y > 2 && playerTransform.position.x > -9 && playerTransform.position.x < -8)
+        {
+            playerTransform.position = new Vector3(-6, 15, 0);
+            cameraTransform.position = new Vector3(0f, 20, -1);
+
+        }
+        //플레이어 main -> forest 이동
+        else if (playerTransform.position.y == -9 && playerTransform.position.x > 2.5 && playerTransform.position.x < 3.5)
+        {
+            playerTransform.position = new Vector3(3, -16, 0);
+            cameraTransform.position = new Vector3(3.5f, -20, -1);
+            
+        }
+       //플레이어 forest -> flower 이동
+       else if(playerTransform.position.x < 12 && playerTransform.position.x >11.7 && playerTransform.position.y > -23.5 && playerTransform.position.y < -22)
+        {
+            playerTransform.position = new Vector3(22, -22, 0);
+            cameraTransform.position = new Vector3(30, -20, -1);
+        }
+       // 플레이어 flower -> shack 이동
+       else if(playerTransform.position.x < 39 && playerTransform.position.x > 38.7 && playerTransform.position.y > -21.5 && playerTransform.position.y < -20)
+        {
+            playerTransform.position = new Vector3(51, -22, 0);
+            cameraTransform.position = new Vector3(60, -20, -1);
+        }
+        // 플레이 shack -> 할머니집 이동
+        else if (playerTransform.position.x < 65.5 && playerTransform.position.x > 64.5 && playerTransform.position.y > -19.5 && playerTransform.position.y < -19)
+        {
+            playerTransform.position = new Vector3(67, -5, 0);
+            cameraTransform.position = new Vector3(73, 0, -1);
+        }
+        else
+        {
+            incameraCharacter();
+        }
     }
 
- 
+    //캐릭터가 카메라 화면에서 못 벗어나는 코드
+    void incameraCharacter()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 0f) pos.x = 0f;
+        if (pos.x > 1f) pos.x = 1f;
+        if (pos.y < 0f) pos.y = 0f;
+        if (pos.y > 1f) pos.y = 0.9f;
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+        
 }
+ 
