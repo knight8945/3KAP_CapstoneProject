22947103e26a20043ff37a7236596c36bf435aa9
201713799KimTaeYoung <126853336+201713799KimTaeYoung@users.noreply.github.com>
2023-05-24 20:19:57 +0900

    김태영
    현재 ObjData에서 오브젝트마다 할당된 id를 가져와서 대사를 출력하는데
    대사가 끝나거나 퀘스트 완료시 id를 변경하여서 talkData에 있는 대사를 출력할려고 하는중 objData의 id가 변하지 않는 문제 수정중

--- AR Fairytale/Assets/script/GameManager.cs
+++ AR Fairytale/Assets/script/GameManager.cs
@@ -12,52 +12,55 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI talkText;
     public GameObject scanObject;
     public bool isAction;
-    public int talkIndex;
+    public int talkIndex = 0;
+    public bool next = false;
 
     private void Start()
     {
-        Debug.Log(questManager.CheckQuest());
+       //Debug.Log(questManager.CheckQuest());
     }
     public void Action(GameObject scanObj)
     {
 
         isAction = true;
-        //talkPanel.SetActive(true);
         scanObject = scanObj;
-        objData objData = scanObject.GetComponent<objData>();
+        ObjData objData = scanObject.GetComponent<ObjData>();
         Talk(objData.id, objData.isNPC);
-        
-        talkPanel.SetActive(isAction); // isAction 과 talkPanel 이 서로 true면 true/ false면 false 로 같으니 하나로 정리
+        talkPanel.SetActive(isAction);
     }
     void Talk(int id, bool isNPC)
     {
         //Set Talk Data
         int questTalkIndex = questManager.GetQuestTalkIndex(id);
-        string talkData = talkManager.GetTalk(id+ questTalkIndex, talkIndex);
+        string talkData = talkManager.GetTalk(id, talkIndex);
 
+       
         //End Talk
         if(talkData == null)
-        {
+        { 
             isAction = false;
             talkIndex = 0;
-            questManager.CheckQuest(id);
+            questManager.CheckQuest();
             return;
         }
 
         if (isNPC)
         {
+
             talkText.text = talkData.Split(':')[0];
             portraitImg.sprite = talkManager.GetPortrait(id, int.Parse(talkData.Split(':')[1]));
             portraitImg.color = new Color(1, 1, 1, 1);
+            
         }
         else
         {
             talkText.text = talkData;
-
             portraitImg.color = new Color(1, 1, 1, 0);
         }
 
         isAction = true;
         talkIndex++;
     }
+
+
 }
--- AR Fairytale/Assets/script/objData.cs
+++ AR Fairytale/Assets/script/objData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class objData : MonoBehaviour
+public class ObjData : MonoBehaviour
 {
     public int id;
     public bool isNPC;
