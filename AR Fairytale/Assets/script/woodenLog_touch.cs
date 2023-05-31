dfc34ae69943bc998aae917208a36ff3fd081d29
son7877 <79892287+son7877@users.noreply.github.com>
2023-05-31 11:39:28 +0900

    안홍범

--- AR Fairytale/Assets/script/woodenLog_touch.cs
+++ AR Fairytale/Assets/script/woodenLog_touch.cs
@@ -2,29 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class woodenLog_touch : MonoBehaviour
 {
-    public GameObject wood;
-    public GameObject check;
-    private int touchCount = 0;
-    public Button touchButton;
-
-    void Start()
+    public void LoadScene()
     {
-        touchButton.onClick.AddListener(ButtonTouch);
-        check.SetActive(false);
-    }
-    public void ButtonTouch()
-    {
-        touchCount++;
-
-        // 10번의 버튼 터치 이후 오브젝트를 비활성화합니다.
-        if (touchCount >= 10 && wood != null)
+        if (Input.touchCount > 10)
         {
-            wood.SetActive(false);
-            check.SetActive(true);
+            Touch touch = Input.GetTouch(0);
+            SceneManager.LoadScene(1);
         }
     }
 }
