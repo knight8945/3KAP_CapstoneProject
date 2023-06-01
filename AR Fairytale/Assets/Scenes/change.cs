c9a8b9295bf2696fede52290d81d34ea313eab57
knight8945 <95391849+knight8945@users.noreply.github.com>
2023-06-01 21:16:15 +0900

    강동훈
    씬 전환 수정

--- AR Fairytale/Assets/Scenes/change.cs
+++ AR Fairytale/Assets/Scenes/change.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
 
 
 public class change : MonoBehaviour
@@ -9,12 +10,10 @@ public class change : MonoBehaviour
     private void Awake()
     {
         SceneManager.SetActiveScene(SceneManager.GetSceneByName("start"));
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("ending"));
-        SceneManager.SetActiveScene(SceneManager.GetSceneByName("tree"));
     }
     public void LoadScene()
     {
-        //SceneManager.UnloadScene("start");
-        //EditorSceneManager.CloseScene(SceneManager.GetSceneByName("start"), true);
+        //SceneManager.UnloadSceneAsync("start");
+        EditorSceneManager.CloseScene(SceneManager.GetSceneByName("start"), true);
     }
 }
